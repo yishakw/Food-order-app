b0c0918b4780e28d019c14ed750395f355486461
Yishak Wondimgezahu <yishakwondimgezahu12@gmail.com>
2023-10-08 19:21:27 +0300

    Added the Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Header from "./Components/Layout/Header";
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import classes from "./Header.module.css";
+import mealsImg from "../../assets/meals.jpg";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="table full of food" />
+      </div>
+    </Fragment>
+  );
+};
+export default Header;

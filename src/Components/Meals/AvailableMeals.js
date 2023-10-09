d40c839a1de0ff51213408da74d5e9ca2044254b
Yishak Wondimgezahu <yishakwondimgezahu12@gmail.com>
2023-10-09 08:30:06 +0300

    Added the MealItem and Card components

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import classes from "./AvailableMeals.module.css";
-
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -28,10 +29,19 @@ const DUMMY_MEALS = [
 ];
 
 function AvailableMeals() {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 }
---
+++ src/Components/Meals/MealItem/MealItem.js
@@ -0,0 +1,18 @@
+import classes from "./MealItem.module.css";
+
+function MealItem(props) {
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li className={classes.meal}>
+      <div>
+        <div>
+          <h3>{props.name}</h3>
+        </div>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+    </li>
+  );
+}
+
+export default MealItem;
